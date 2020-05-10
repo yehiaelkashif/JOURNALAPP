61b26fb65f718562d7020cc8a1660eea439fe43d
yehia <yehia.elkashif@yahoo.com>
2020-05-10 09:24:34 +0200

    fd

--- app/src/main/java/com/example/self/MainActivity.java
+++ app/src/main/java/com/example/self/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.self;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.example.self.util.JournalApi;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
